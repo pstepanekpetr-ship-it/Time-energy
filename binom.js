574ca9c1c33c793399c20d87a55d8e7729822309
pstepanekpetr-ship-it <p.stepanekpetr@gmail.com>
2026-09-16 19:45:48 +0200

    Update binom.js

--- binom.js
+++ binom.js
@@ -1,10 +1,10 @@
-const CAPITAL_CYCLE = ['K', 'L', 'F', 'E', 'C', 'D', 'B', 'A', 'G', 'H'];
-const LOWERCASE_CYCLE = ['k', 'l', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+const HEAVEN_STEM_CYCLE = ['K', 'L', 'F', 'E', 'C', 'D', 'B', 'A', 'G', 'H'];
+const EARTHLY_BRANCH_CYCLE = ['k', 'l', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
 
-// Anchor: 2026-01-01 = "Lj" (capital index 1, lowercase index 11), verified against source calendar.
+// Anchor: 2026-01-01 = "Lj" (heaven stem index 1, earthly branch index 11), verified against source calendar.
 const ANCHOR_UTC_MS = Date.UTC(2026, 0, 1);
-const ANCHOR_CAPITAL_INDEX = 1;
-const ANCHOR_LOWERCASE_INDEX = 11;
+const ANCHOR_HEAVEN_STEM_INDEX = 1;
+const ANCHOR_EARTHLY_BRANCH_INDEX = 11;
 const MS_PER_DAY = 86400000;
 
 function mod(n, m) {
@@ -16,12 +16,12 @@ function getBinom(date) {
   const dayUTC = Date.UTC(date.getFullYear(), date.getMonth(), date.getDate());
   const diffDays = Math.round((dayUTC - ANCHOR_UTC_MS) / MS_PER_DAY);
 
-  const capital = CAPITAL_CYCLE[mod(ANCHOR_CAPITAL_INDEX + diffDays, 10)];
-  const lowercase = LOWERCASE_CYCLE[mod(ANCHOR_LOWERCASE_INDEX + diffDays, 12)];
+  const heavenStem = HEAVEN_STEM_CYCLE[mod(ANCHOR_HEAVEN_STEM_INDEX + diffDays, 10)];
+  const earthlyBranch = EARTHLY_BRANCH_CYCLE[mod(ANCHOR_EARTHLY_BRANCH_INDEX + diffDays, 12)];
 
-  return capital + lowercase;
+  return heavenStem + earthlyBranch;
 }
 
 if (typeof module !== 'undefined' && module.exports) {
-  module.exports = { getBinom, CAPITAL_CYCLE, LOWERCASE_CYCLE };
+  module.exports = { getBinom, HEAVEN_STEM_CYCLE, EARTHLY_BRANCH_CYCLE };
 }
